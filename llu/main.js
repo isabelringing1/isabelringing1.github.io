e422d137360aba8e4f01e3d8da0b019cf689c9d4
farmerisabel <isabel@doubleloopgames.com>
2022-10-04 22:59:18 -0400

    Added images

--- llu/main.js
+++ llu/main.js
@@ -1,23 +1,50 @@
-var images = ["bg.png", "bg.png", "bg.png"];
-
 var startingY = -1;
 var lastY = -1;
 var percentToSwipe = 15;
 var scrolling = false;
 var scrollTimeout;
 var isMoving = false;
+var map;
 
 var currentPage = 0;
 var pages =[];
 
 // On Page Load
 $(function() {
-    for (var i = 0; i < images.length; i++){
-        var img = $('<img class = "image" id="image' + i + '" draggable="true">');
-        img.attr('src', "../llu/images/" + images[i]);
-        img.appendTo('#container');
-        pages.push(img);
-    }
+    $.getJSON('data.json', function(jsonData, status, xhr){
+        data = jsonData;
+        map = new Map();
+        artCaptionMap = new Map();
+        for (var i = 0; i < data.data.length; i++){
+            var bg = data.data[i].background;
+            var overlays = data.data[i].overlays;
+            var text = data.data[i].text;
+            map.set(bg, [overlays, text]);
+
+            var component = $('<div class = "component" id="component' + i + '" draggable="true">');
+            component.appendTo('#container');
+
+            var img = $('<img class = "image" id="image' + i + '">');
+            img.attr('src', "../llu/images/" + bg);
+            img.appendTo('#component' + i);
+
+            for (var j = 0; j < overlays.length; j++){
+                var overlay = $('<img class = "overlay" id="overlay' + i + "." + j + '">');
+                overlay.attr('src', "../llu/images/" + overlays[j]);
+                overlay.appendTo('#component' + i);
+                console.log(overlay[0].width);
+                overlay[0].style.left += 450 * j;
+            }
+            for (var j = 0; j < text.length; j++){
+                var textOverlay = $('<img class = "text" id="text' + i + "." + j + '">');
+                textOverlay.attr('src', "../llu/images/" + text[j]);
+                textOverlay.appendTo('#component' + i);
+            }
+
+            pages.push(img);
+        }
+        console.log(map);
+    });
 
     document.addEventListener("drag", drag);
     document.addEventListener("dragend", dragEnd);
@@ -87,7 +114,7 @@ function moveEnd(){
     var difference = startingY - lastY;
     var percentDragged = 100 * (difference / screen.height);
     console.log("percent dragged: " + percentDragged);
-    if (percentDragged > percentToSwipe && currentPage + 1 < images.length){
+    if (percentDragged > percentToSwipe && currentPage + 1 < pages.length){
         currentPage++;
     }
     else if (percentDragged < -percentToSwipe && currentPage != 0){
