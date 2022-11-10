2055268d46806a62d5c40632b047a6c3e5b20ef4
farmerisabel <isabel@doubleloopgames.com>
2022-11-10 11:02:49 -0500

    More updates

--- old-house/main.js
+++ old-house/main.js
@@ -1,4 +1,4 @@
-var photos = ["house1.png", "house2.png", "house3.png", "house4.png"];
+var photos = ["house1.png", "house2.png", "house3.png", "house4.png", "kitchen1.jpg", "kitchen2.jpg", "kitchen3.jpg", "kitchen4.jpg", "dog1.png", "dog2.png", "dog3.png", "dog4.png"];
 
 var curr = 0;
 
@@ -9,7 +9,7 @@ $(function() {
     $(window).bind('mousewheel', function(event, delta) {
         console.log(document.body.scrollTop);
         var i = Math.floor(document.body.scrollTop / 307);
-        if (i != curr){
+        if (i != curr && i < photos.length){
             curr = i;
             console.log("Changing index to " + i);
             $("#photo").attr("src", "../old-house/images/" + photos[curr]);
